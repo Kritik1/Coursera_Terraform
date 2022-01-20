783afe6c85d50bfd62f97675af790fd838e7e49a
kritik1 <kritikac783@gmail.com>
2022-01-20 09:57:13 +0000

    adding storage account

---
+++ storageacc.tf
@@ -0,0 +1,17 @@
+resource "random_id" "randomId" {
+          keepers = {
+              resource_group = azurerm_resource_group.sampleResourceGroup.name
+        }
+       byte_length = 8
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "diag${random_id.randomId.hex}"
+  resource_group_name      = azurerm_resource_group.sampleResourceGroup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
+}
